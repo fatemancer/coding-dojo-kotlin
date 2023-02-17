4371c6f4ee09afecf32019a335bd71d44352c30d
timmson <timmson666@mail.ru>
2023-02-18 01:15:30 +0300

    Upgraded Gradle to 8 and Kotlin 1.8

--- src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
+++ src/test/kotlin/ru/technicalExcellence/codingDojo/HelloWorldShould.kt
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test
 class HelloWorldShould {
 
     @Test
-    fun returnHelloWorld() {
-        val expected = "Hello World"
-
-        val actual = HelloWorld().helloWorld()
-
-        assertEquals(expected, actual)
+    fun `Sum of Squared Inputs`() {
+        assertEquals(5, squareSum(arrayOf(1, 2)))
+        assertEquals(50, squareSum(arrayOf(0, 3, 4, 5)))
+        assertEquals(0, squareSum(arrayOf()))
     }
+
+    private fun squareSum(arrayOf: Array<Int>): Int = arrayOf.sumOf { it * it }
 }
 
